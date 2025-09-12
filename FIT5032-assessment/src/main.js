d967a9878a9cd2a67dc74506e53c9c689efe7243
ltx <tlin0048@student.monash.edu>
2025-09-12 22:34:32 +1000

    import firebase

--- FIT5032-assessment/src/main.js
+++ FIT5032-assessment/src/main.js
@@ -10,6 +10,26 @@ import router from './router'
 //import bootstrap css
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBybroavfNOvEShCudmCH-MoPXiDTCHn94",
+  authDomain: "fit5032-assessment-16c0f.firebaseapp.com",
+  projectId: "fit5032-assessment-16c0f",
+  storageBucket: "fit5032-assessment-16c0f.firebasestorage.app",
+  messagingSenderId: "73095703349",
+  appId: "1:73095703349:web:2eed851e2380cd0164ba7c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 createApp(App)
 .use(router)
 .mount('#app')
